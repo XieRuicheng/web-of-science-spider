0e2608c790509ac8c19d218303d83ef2bd742acc
Xie Ruicheng <x_rc@qq.com>
2020-10-17 11:17:54 +0800

    Feat: set non-blocking model and explict watit

--- wos_spider.py
+++ wos_spider.py
@@ -19,7 +19,12 @@ import logging
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
-from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import TimeoutException, ElementNotInteractableException
 
 
 # logging configuration
@@ -46,13 +51,16 @@ logger.addHandler(fhlr)
 # url
 root_url = 'https://apps.webofknowledge.com'
 
+desired_capabilities = DesiredCapabilities.CHROME
+desired_capabilities["pageLoadStrategy"] = "none"
+
 # start browser
 def initial_driver():
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
     driver = webdriver.Chrome(options=options)
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(30)
     driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
         "source": """
         Object.defineProperty(navigator, 'webdriver', {
@@ -134,20 +142,50 @@ def back_to_main():
 
 def go_to_next_cite():
     # page number
-    page_num = driver.find_element_by_id('paginationForm2').text
-    logger.info(f'Processing citation in {page_num}')
-    # click next page
-    driver.find_element_by_css_selector('#paginationForm2 > span > a.paginationNext.snowplow-navigation-nextpage-bottom').click()
+    page_num_text = driver.find_element_by_id('paginationForm2').text
+    logger.info(f'Processing citation in {page_num_text}')
+
+    page_group = re.search('第 (\d+) 条，共 (\d+) 条', page_num_text)
+    if page_group:
+        current_page = int(page_group.group(1))
+        total_page = int(page_group.group(2))
+    else:
+        logger.error('Page RE of ID do not match')
+        return 1
+
+    if current_page < total_page:
+        # click next page
+        driver.find_element_by_css_selector(
+            '#paginationForm2 > span > a.paginationNext.snowplow-navigation-nextpage-bottom'
+        ).click()
+    else:
+        logger.debug('In the last page')
+
+    return 0
 
 
-def random_sleep(mu=3, sigma=1, min=1):
+def random_sleep(mu=5, sigma=2, min_time=3, max_time=15):
     sleep_time = random.normalvariate(mu, sigma)
-    sleep_time = max(sleep_time, min)
+    sleep_time = max(sleep_time, min_time)
+    sleep_time = min(sleep_time, max_time)
+    logger.debug(f'Sleeping {sleep_time}s')
     time.sleep(sleep_time)
 
 
 def verify_title(web_title, given_title):
     return True
+
+
+def wait_find(driver, loc_m, loc):
+    return WebDriverWait(driver, 120).until(EC.visibility_of_element_located((loc_m, loc)))
+
+
+def wait_find_all(driver, loc_m, loc):
+    try:
+        elem = WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located((loc_m, loc)))
+    except TimeoutException:
+        elem = []
+    return elem
 
 
 def get_cite_detail(record_detail_text):
@@ -231,7 +269,7 @@ def search_paper_info(paper, record_folder):
     record_title_text = None # paper title
     record_brief_text = 'NA' # brief information of paper
 
-    record_chunks = driver.find_elements_by_class_name('search-results-item')
+    record_chunks = wait_find_all(driver, By.CLASS_NAME, 'search-results-item')
     for idx, record in enumerate(record_chunks):
         # text of whole record, include title, author, journal, data, citation num
         record_brief_text = record.text
@@ -243,8 +281,8 @@ def search_paper_info(paper, record_folder):
             # add record to monitor to obtain H-index
             add_to_monitor(idx + 1)
             # record detail link
-            detail_link = driver.find_elements_by_class_name('search-results-item')[idx]
-            detail_link = detail_link.find_element_by_xpath('div[3]/div/div[1]/div/a')
+            detail_link = wait_find_all(driver, By.CLASS_NAME, 'search-results-item')[idx]
+            detail_link = wait_find(detail_link, By.XPATH, 'div[3]/div/div[1]/div/a')
             break
 
     # no matched paper found
@@ -273,7 +311,7 @@ def search_paper_info(paper, record_folder):
     # ------------------------------------------------------------ #
 
     # text of whole record page
-    record_detail_text = driver.find_element_by_id('records_form').text
+    record_detail_text = wait_find(driver, By.ID, 'records_form').text
 
     # journal IF
     impact_factor, jcr_rank, _ = get_journal_info(idx + 1)
@@ -304,17 +342,25 @@ def search_paper_info(paper, record_folder):
         back_to_main()
         return search_result
 
-    random_sleep(mu=1)
+    random_sleep()
 
     # ------------------------------------------------------------ #
     # follow link if the paper is cited
     # ------------------------------------------------------------ #
 
     # click citation number
-    driver.find_element_by_css_selector('.flex-row-partition1 > a').click()
+    wait_find(driver, By.CSS_SELECTOR, '.flex-row-partition1 > a').click()
     random_sleep()
-    # go to the detail page of first cite paper
-    driver.find_element_by_xpath('//*[@id="RECORD_1"]/div[3]/div/div[1]/div/a').click()
+
+    # go to the detail page of first cite paper, if citation library is book
+    cite_chunks = driver.find_elements_by_xpath('//*[@id="RECORD_1"]/div[3]/div/div[1]/div/a')
+    if len(cite_chunks) > 0:
+        cite_chunks[0].click()
+    elif len(driver.find_elements_by_id('noRecordsDiv')) > 0:
+        cite_error_text = driver.find_elements_by_id('noRecordsDiv')
+        logger.warning(f'Citation is not booked: {cite_error_text[0].text}')
+    else:
+        logger.error('Citation page abnormal')
     random_sleep()
 
     # ------------------------------------------------------------ #
@@ -324,12 +370,12 @@ def search_paper_info(paper, record_folder):
     # cite info
     cite_detail = []
     for aa in range(cite_num):
-        cite_detail_text = driver.find_element_by_id('records_form').text
+        cite_detail_text = wait_find(driver, By.ID, 'records_form').text
+        # cite_detail_text = driver.find_element_by_id('records_form').text
         cite_info = get_cite_detail(cite_detail_text)
         cite_detail.append(cite_info)
-        if aa < cite_num-1:
-            go_to_next_cite()
-        random_sleep(mu=5)
+        go_to_next_cite()
+        random_sleep(mu=8)
 
     search_result['CiteDetail'] = cite_detail
 
@@ -352,18 +398,16 @@ if __name__ == '__main__':
     with open('杨云云发表论文清单.txt', 'r', encoding='utf8') as fp:
         paper_list = fp.readlines()
         paper_list = [x.strip() for x in paper_list]
-    # paper_list = [
-    #     'Ultrasound pupil image segmentation based on edge detection and detection operators',
-    #     'Level set formulation for automatic medical image segmentation based on fuzzy clustering',
-    #     'A Novel Clustering Method for Static Video Summarization',
-    #     'Split Bregman Method for Minimization of Fast Multiphase Image Segmentation Model for Inhomogeneous Images',
-    # ]
 
     # make record folder
     record_folder = f'./logs/{time_stamp}'
     os.mkdir(record_folder)
 
+    total_paper = len(paper_list)
     result_list = []
-    for paper in paper_list:
+    for aa, paper in enumerate(paper_list):
+        if aa < 24:
+            continue
+        logger.info(f'Processing {aa} in {total_paper}')
         result = search_paper_info(paper, record_folder)
         result_list.append(result)
